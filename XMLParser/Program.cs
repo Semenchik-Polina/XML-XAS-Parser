7d4be19dcd73058ed0c9e657d22741541c852c3f
Semenchik-Polina <36949280+Semenchik-Polina@users.noreply.github.com>
2018-06-07 19:23:16 +0300

    added the basis for parsing

--- XMLParser/Program.cs
+++ XMLParser/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,34 @@ namespace XMLParser
     {
         static void Main(string[] args)
         {
+            string path = "C://all you need is//to study//ООП//6lab//feed-test.xml";
+            try
+            {
+                int male = 0;
+                int female = 0;
+                using (XmlTextReader reader = new XmlTextReader(path))
+                {
+                    reader.WhitespaceHandling = WhitespaceHandling.None;
+                    bool rightCity = false;
+                    
+                    while (reader.Read())
+                    {
+                        if (reader.NodeType == XmlNodeType.Element && reader.LocalName == "city")
+                        
+
+                        if (reader.NodeType == XmlNodeType.EndElement && reader.LocalName == "user")
+                        {
+
+                        }
+                    }
+                    
+                }
+                Console.WriteLine("male: {0}, female: {1}", male, female);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
