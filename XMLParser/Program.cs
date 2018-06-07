807dd0d877ba586028ddb5eac21cd072425437ea
Semenchik-Polina <36949280+Semenchik-Polina@users.noreply.github.com>
2018-06-07 23:45:40 +0300

    find number of men/women by city name

--- XMLParser/Program.cs
+++ XMLParser/Program.cs
@@ -11,29 +11,53 @@ namespace XMLParser
     {
         static void Main(string[] args)
         {
-            string path = "C://all you need is//to study//ООП//6lab//feed-test.xml";
+            string path = @"C:\all you need is\to study\ООП\6lab\feed.xml";
+            Console.WriteLine("Please enter city name");
+
             try
             {
-                int male = 0;
-                int female = 0;
+                //    string temp = Console.ReadLine();
+                //    string[] city;
+                string city = Console.ReadLine();
+                int maleCount = 0;
+                int femaleCount = 0;
                 using (XmlTextReader reader = new XmlTextReader(path))
                 {
                     reader.WhitespaceHandling = WhitespaceHandling.None;
                     bool rightCity = false;
+                    bool inGender = false;
+                    bool inCity = false;
                     
                     while (reader.Read())
                     {
                         if (reader.NodeType == XmlNodeType.Element && reader.LocalName == "city")
+                            inCity = true;
+
+                        if (inCity && reader.NodeType == XmlNodeType.Text && reader.Value == city)
+                            rightCity = true;
                         
+                        if (rightCity && reader.NodeType == XmlNodeType.Element && reader.LocalName == "gender")
+                            inGender = true;
+
+                        if (reader.NodeType == XmlNodeType.Text && inGender)
+                        {
+                            if (reader.Value == "male")
+                                maleCount++;
+                            if (reader.Value == "female")
+                                femaleCount++;
+                        }
 
                         if (reader.NodeType == XmlNodeType.EndElement && reader.LocalName == "user")
                         {
-
+                            rightCity = false;
+                            inCity = false;
+                            inGender = false;
                         }
                     }
                     
                 }
-                Console.WriteLine("male: {0}, female: {1}", male, female);
+                Console.WriteLine("male: {0}, female: {1}", maleCount, femaleCount);
+                Console.ReadKey();
             }
             catch(Exception ex)
             {
